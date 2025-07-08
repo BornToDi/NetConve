e886a20bcc04f7fe6cd33f66451b2039f3dbb61a
Nayeem Islam <z.i.nayeem45@gmail.com>
2025-07-08 19:12:35 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className={`${inter.variable} font-body antialiased`}>
+      <body className={`${inter.variable} font-body antialiased`} suppressHydrationWarning>
         {children}
         <Toaster />
       </body>
